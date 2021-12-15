1cbbc4605d7a1ff10f77e9910ef3d16ed5db8414
Matheus Gomes <mth.gom@gmail.com>
2021-12-15 16:06:30 -0300

    Create POST

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 // import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
@@ -17,8 +18,16 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
-  } catch (err) {
+    const { title, value, type } = request.body;
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({ title, value, type });
+
+    return response.json(transaction);
+  } catch (err: any) {
     return response.status(400).json({ error: err.message });
   }
 });
